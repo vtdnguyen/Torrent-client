744de4ee03640d5683a009f7068a0c50b62c3045
vtdnguyen <89002918+vtdnguyen@users.noreply.github.com>
2025-07-01 07:54:38 +0700

    commit first code

---
+++ testing.py
@@ -0,0 +1,19 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+print("This is jonas file...")
+
+file_path = "malware.txt"
+
+with open(file_path, "w") as file:
+    file.write("X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*")
+
+print(f"File '{file_path}' created and content written successfully.")
