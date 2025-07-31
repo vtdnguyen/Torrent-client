37d3a95a0ce225d2958a766f07599b7a547f9372
vtdnguyen <89002918+vtdnguyen@users.noreply.github.com>
2025-07-31 10:21:36 +0700

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+  },
+};
